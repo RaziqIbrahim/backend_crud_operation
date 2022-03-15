297b1b002bc08edcc7ed0e2664487e2148fbd4d0
RaziqIbrahim <97674322+RaziqIbrahim@users.noreply.github.com>
2022-03-15 18:56:36 +0530

    Added service files

---
+++ backendTest/backendTest/Services/UserServices/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using backendTest.BOs;
+using backendTest.Helpers;
+
+namespace backendTest.Services.UserServices
+{
+    public interface IUserService
+    {
+        public ServiceResult<List<UserBo>> get_all_user(string username);
+
+        public ServiceResult<bool> insertUserData(UserBo userDetails);
+
+        public ServiceResult<bool> deleteUserData(string user_name);
+
+        public ServiceResult<bool> updateUserData(UserBo userDetails);
+    }
+}
---
+++ backendTest/backendTest/Services/UserServices/UserServices.cs
@@ -0,0 +1,138 @@
+﻿using backendTest.BOs;
+using backendTest.Helpers;
+using backendTest.Helpers.DataStores;
+
+namespace backendTest.Services.UserServices
+{
+    public class UserServices : IUserService
+    {
+        public ServiceResult<List<UserBo>> get_all_user(string username)
+        {
+            try
+            {
+                List<UserBo> users = new List<UserBo>();
+                users=UserSchema.get_all_users(username);
+                return new ServiceResult<List<UserBo>>()
+                {
+                    Status = ServiceStatus.Success,
+                    Message = "Got all the feedback",
+                    Content = users
+                };
+            }catch (Exception ex)
+            {
+                return new ServiceResult<List<UserBo>>()
+                {
+                    Status = ServiceStatus.Failed,
+                    Message = ex.Message,
+                    Content = null
+                };
+            }
+        }
+
+        public ServiceResult<bool> insertUserData(UserBo UserDeatils)
+        {
+            try
+            {
+                var res = UserSchema.insertUserData(UserDeatils);
+                if (res > 0)
+                {
+                    return new ServiceResult<bool>()
+                    {
+                        Status = ServiceStatus.Success,
+                        Message = "Inserted user data",
+                        Content = true
+                    };
+                }
+                else
+                {
+                    return new ServiceResult<bool>()
+                    {
+                        Status = ServiceStatus.Failed,
+                        Message = "Unable to Insert",
+                        Content = false
+                    };
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResult<bool>()
+                {
+                    Status = ServiceStatus.Failed,
+                    Message = ex.Message,
+                    Content = false
+                };
+            }
+        }
+
+        public ServiceResult<bool> deleteUserData(string user_name)
+        {
+            try
+            {
+                var res = UserSchema.deleteUserData(user_name);
+                if (res > 0)
+                {
+                    return new ServiceResult<bool>()
+                    {
+                        Status = ServiceStatus.Success,
+                        Message = "Inserted user data",
+                        Content = true
+                    };
+                }
+                else
+                {
+                    return new ServiceResult<bool>()
+                    {
+                        Status = ServiceStatus.Failed,
+                        Message = "Unable to Insert",
+                        Content = false
+                    };
+                }
+            }catch(Exception ex)
+            {
+                return new ServiceResult<bool>()
+                {
+                    Status = ServiceStatus.Failed,
+                    Message = ex.Message,
+                    Content = false
+                };
+            }
+           
+        }
+
+        public ServiceResult<bool> updateUserData(UserBo userDetails)
+        {
+            try
+            {
+                var res=UserSchema.updateUserData(userDetails);
+                if (res > 0)
+                {
+                    return new ServiceResult<bool>()
+                    {
+                        Status = ServiceStatus.Success,
+                        Message = "Inserted user data",
+                        Content = true
+                    };
+                }
+                else
+                {
+                    return new ServiceResult<bool>()
+                    {
+                        Status = ServiceStatus.Failed,
+                        Message = "Unable to Insert",
+                        Content = false
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResult<bool>()
+                {
+                    Status = ServiceStatus.Failed,
+                    Message = ex.Message,
+                    Content = false
+                };
+            }
+        }
+    }
+}
